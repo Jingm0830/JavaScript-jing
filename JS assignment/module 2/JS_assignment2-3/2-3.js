835ea6f78c9b7e385023b23cf3c9dd0115668044
8.25 Amir <jingm@metropolia.fi>
2023-11-26 23:02:59 +0200

    2-3

--- JS assignment/module 2/JS_assignment2-3/2-3.js
+++ JS assignment/module 2/JS_assignment2-3/2-3.js
@@ -1,16 +1,16 @@
 'use strict'
 
-let name=[];
-let number=6;
+let name = [];
+let number = 6;
 
-for (let i=0; i<number; i++){
-    name.push(prompt('Enter the name of dog No.${i+1}'))
+for (let i = 0; i < number; i++) {
+    name.push(prompt('Enter the name of dog '))
 }
 name.sort().reverse()
 
-const ul=document.querySelector('#dog');
-for(let i=0; i<name.length; i++){
-    const li=document.createElement('li');
-    li.innerText=name[i];
-    ul.appendChild()
+const ul = document.querySelector('#dog');
+for (let i = 0; i < name.length; i++) {
+    const li = document.createElement('li');
+    li.innerText = name[i];
+    ul.appendChild(li)
 }
