bd303d58ab503d3d4e8642a8767f1b13a7a5376e
8.25 Amir <jingm@metropolia.fi>
2023-11-26 21:54:27 +0200

    2-2

---
+++ JS assignment/module 2/JS_assignment2-2/2-2.js
@@ -0,0 +1,14 @@
+'use strict';
+const num = parseInt(prompt('please enter the num of participants:'));
+let nameArray = [];
+for (let i = 0; i < num; i++) {
+  nameArray.push(prompt('please enter the name:'));
+}
+nameArray.sort();
+
+const ol = document.querySelector('#P_name');
+for (let i = 0; i < nameArray.length; i++) {
+  const li = document.createElement('li');
+  li.innerText = nameArray[i];
+  ol.appendChild(li);
+}
