d0f50a5edb5900d50bdd59664e40236993ab1e9d
8.25 Amir <jingm@metropolia.fi>
2023-11-28 22:22:49 +0200

    module3

---
+++ JS assignment/module_3/t2/2.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const li1 = document.createElement('li')
+li1.innerHTML = 'First item';
+document.querySelector('#target').appendChild(li1);
+
+const li2 = document.createElement('li')
+li2.innerHTML = 'Second item';
+li2.setAttribute('class','my-item');
+document.querySelector('#target').appendChild(li2);
+
+const li3 = document.createElement('li')
+li3.innerHTML = 'Third item';
+document.querySelector('#target').appendChild(li3);
